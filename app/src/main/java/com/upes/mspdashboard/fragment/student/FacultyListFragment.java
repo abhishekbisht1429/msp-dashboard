b2f466773d8617816d172da6552c3098ed298dec
Abhishek Bisht <bishtabhishekk11a1@gmail.com>
2019-09-16 09:21:03 +0530

    implemented the faculty list functionality for students

--- app/src/main/java/com/upes/mspdashboard/fragment/student/FacultyListFragment.java
+++ app/src/main/java/com/upes/mspdashboard/fragment/student/FacultyListFragment.java
@@ -4,16 +4,29 @@ package com.upes.mspdashboard.fragment.student;
 import android.content.Context;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.upes.mspdashboard.R;
+import com.upes.mspdashboard.model.Faculty;
+import com.upes.mspdashboard.util.SessionManager;
 
-public class FacultyListFragment extends Fragment {
+import java.util.ArrayList;
+import java.util.List;
 
+public class FacultyListFragment extends Fragment implements
+    SwipeRefreshLayout.OnRefreshListener {
+    private SwipeRefreshLayout swrLayout;
+    private RecyclerView rv;
+    private RVAdapter rvAdapter;
     OnFragmentInteractionListener mListener;
     public FacultyListFragment() {
         // Required empty public constructor
@@ -38,11 +51,64 @@ public class FacultyListFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_faculty_list, container, false);
+        View view = inflater.inflate(R.layout.fragment_faculty_list, container, false);
+        swrLayout = view.findViewById(R.id.swr_layout_fac_list);
+        rv = view.findViewById(R.id.rv_fac_list);
+        rv.setLayoutManager(new LinearLayoutManager(this.getContext()));
+        rvAdapter = new RVAdapter();
+        rv.setAdapter(rvAdapter);
+        Faculty demo = new Faculty.Builder()
+                .username("abhishek")
+                .password("pass")
+                .build();
+        rvAdapter.add(demo);
+        rvAdapter.add(demo);
+        return view;
+    }
+
+    @Override
+    public void onRefresh() {
+
     }
 
     public interface OnFragmentInteractionListener {
 
     }
+    static class FacultyVH extends RecyclerView.ViewHolder {
+        private TextView tvName;
+        public FacultyVH(@NonNull View itemView) {
+            super(itemView);
+            tvName = itemView.findViewById(R.id.txtv_fac_list_name);
+        }
 
+        public void bind(Faculty faculty) {
+            tvName.setText(faculty.getUsername());
+        }
+    }
+    static class RVAdapter extends RecyclerView.Adapter<FacultyVH> {
+        private List<Faculty> facultyList = new ArrayList<>();
+        @NonNull
+        @Override
+        public FacultyVH onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_layout_fac_list,parent,false);
+            return new FacultyVH(view);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull FacultyVH holder, int position) {
+            holder.bind(facultyList.get(position));
+        }
+
+        @Override
+        public int getItemCount() {
+            return facultyList.size();
+        }
+
+        public void add(Faculty faculty) {
+            if(faculty!=null) {
+                facultyList.add(faculty);
+                notifyItemChanged(facultyList.size() - 1);
+            }
+        }
+    }
 }
